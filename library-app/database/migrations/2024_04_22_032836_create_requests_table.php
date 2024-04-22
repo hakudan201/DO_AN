93bba85db868057d819514bac2e4d3f535d9e44f
Dang Huy Hoang <doiquanhieumk@gmail.com>
2024-04-22 15:07:35 +0700

    first_commit

---
+++ library-app/database/migrations/2024_04_22_032836_create_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('book_copy_id');
+            $table->date('reserve_date');
+            $table->date('checkout_date');
+            $table->date('due_date');
+            $table->date('return_date');
+            $table->enum('status', ['Reserved', 'Checked Out', 'Returned', 'Lost']);
+            $table->integer('fee')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('book_copy_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
